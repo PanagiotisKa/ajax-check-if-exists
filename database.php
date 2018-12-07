ebfc350438f5e264e39c301bc98c92c92d7aa84a
PanagiotisKa <37039853+PanagiotisKa@users.noreply.github.com>
2018-12-07 12:18:26 +0200

    Update database.php

--- database.php
+++ database.php
@@ -1,5 +1,5 @@
 <?php
-      public function check_coral($code){
+      public function check_email($code){
         $queryText = "SELECT count(*) FROM coral WHERE code='$code'";
         $res = $this->sql->query($queryText);
         return $res;
